ab5c5b9ca75eab139821b0a731aeec3c94f0b3e8
nguyendolar <nguyencaonguyencmu@gmail.com>
2021-11-11 00:02:01 +0700

    up

---
+++ ForumCoderASP/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ForumCoderASP.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        
+        // GET: Authentication
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        
+    }
+}
