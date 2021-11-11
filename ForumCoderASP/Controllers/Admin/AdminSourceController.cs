067b7b5d52812f438dbce065bb03d104853d8df1
minhnhacmu2308 <65648352+minhnhacmu2308@users.noreply.github.com>
2021-11-11 23:28:07 +0700

    new

---
+++ ForumCoderASP/Controllers/Admin/AdminSourceController.cs
@@ -0,0 +1,91 @@
+﻿using ForumCoderASP.Daos;
+using ForumCoderASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForumCoderASP.Controllers.Admin
+{
+    public class AdminSourceController : Controller
+    {
+        CourseDao courseDao = new CourseDao();
+        // GET: AdminSource
+        public ActionResult Index(string mess)
+        {
+            ViewBag.mess = mess;
+            var list = courseDao.getCourses();
+            return View(list);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Add(Course course)
+        {
+            var file = Request.Files["file"];
+            string reName = DateTime.Now.Ticks.ToString() + file.FileName;
+            file.SaveAs(Server.MapPath("~/Content/assets/imgs/" + reName));
+            course.image = reName;
+            course.createdAt = DateTime.Now;
+            course.status = 1;
+            var obj = courseDao.checkExistName(course.name);
+            if(obj != null)
+            {
+                return RedirectToAction("Index", new { mess = "1" });
+            }
+            else
+            {
+                courseDao.add(course);
+                return RedirectToAction("Index", new { mess = "2" });
+            }        
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Update(Course course)
+        {
+            string reName = "";
+            var objCourse = courseDao.getCourseById(course.id_course);
+            var file = Request.Files["file"];
+            if(file.FileName == "")
+            {
+                reName = objCourse.image;
+            }
+            else
+            {
+                reName = DateTime.Now.Ticks.ToString() + file.FileName;
+                file.SaveAs(Server.MapPath("~/Content/assets/imgs/" + reName));
+            }                  
+            course.image = reName;
+            course.createdAt = DateTime.Now;
+            course.status = 1;
+            if (objCourse.name.Equals(course.name))
+            {
+                courseDao.update(course);
+                return RedirectToAction("Index", new { mess = "2" });
+            }
+            else
+            {
+                var obj = courseDao.checkExistName(course.name);
+                if (obj != null)
+                {
+                    return RedirectToAction("Index", new { mess = "1" });
+                }
+                else
+                {
+                    courseDao.update(course);
+                    return RedirectToAction("Index", new { mess = "2" });
+                }
+            }
+           
+        }
+
+        [HttpPost]
+       
+        public ActionResult Delete(Course course)
+        {         
+                courseDao.delete(course.id_course);
+                return RedirectToAction("Index", new { mess = "2" });
+        }
+    }
+}
---
+++ ForumCoderASP/Daos/UserDao.cs
@@ -0,0 +1,79 @@
+﻿using ForumCoderASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace ForumCoderASP.Daos
+{
+    public class UserDao
+    {
+        DBContextForum myDb = new DBContextForum();
+
+        public List<User> getUsers()
+        {
+            return myDb.Users.OrderByDescending(x => x.id_user).ToList(); 
+        }
+
+        public User getUserById(int id)
+        {
+            return myDb.Users.Where(u => u.id_user == id).FirstOrDefault();
+        }
+
+        public void changeStatus(int id)
+        {
+            var user = getUserById(id);
+            user.status = user.status == 1 ? 0 : 1;
+            myDb.SaveChanges();
+        }
+        public void register(User user)
+        {
+            myDb.Users.Add(user);
+            myDb.SaveChanges();
+        }
+
+        public bool checkLogin(string email, string password)
+        {
+            var objUser = myDb.Users.Where(u => u.email == email && u.password == password).FirstOrDefault();
+            if (objUser != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool checkEmailExist(string email)
+        {
+            var objUser = myDb.Users.Where(u => u.email == email).FirstOrDefault();
+            if (objUser != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string md5(string password)
+        {
+            MD5 md = MD5.Create();
+            byte[] inputString = System.Text.Encoding.ASCII.GetBytes(password);
+            byte[] hash = md.ComputeHash(inputString);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x"));
+            }
+            return sb.ToString();
+        }
+        public User getUserByEmail(string email)
+        {
+            var obj = myDb.Users.Where(u => u.email == email).FirstOrDefault();
+            return obj;
+        }
+    }
+}
